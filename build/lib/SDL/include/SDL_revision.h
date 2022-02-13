347ce61cbe5b7c17b031d672fa4b852504b41d1c
Zachary Ross <zachross015@gmail.com>
2022-02-13 01:57:12 -0500

    Fixed submodule dependencies

--- build/lib/SDL/include/SDL_revision.h
+++ build/lib/SDL/include/SDL_revision.h
@@ -1,4 +1,4 @@
-/* #undef SDL_REVISION */
+#define SDL_REVISION "https://github.com/zachross015/zr-ge.git@05b3959a77b78d472bbc96650e00ec0aa22a5dc7"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
